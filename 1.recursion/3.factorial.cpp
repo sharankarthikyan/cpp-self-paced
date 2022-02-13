ab4e191ebe629c1594bbb30c51e1f165f0438ceb
Sharan Karthikeyan <karthikeyansharan00@gmail.com>
2022-02-13 14:18:59 +0530

    factotial

---
+++ 1.recursion/3.factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  std::cout << factorial(n);
+  return 0;
+}
